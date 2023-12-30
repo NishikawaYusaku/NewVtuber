e2ad5baa3f8243e26af896e1e10e2bca01b71c77
NishikawaYusaku <sakurairo447@gmail.com>
2023-12-30 11:18:27 +0900

    [Add]DBスキーマファイル+他変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'boards/index'
+  # get 'boards/index'
+  root 'boards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
