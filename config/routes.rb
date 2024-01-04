3bcbcb20f9ee6435c6c9fddd76f78048255f3f57
NishikawaYusaku <sakurairo447@gmail.com>
2024-01-04 17:15:57 +0900

    [Add]ユーザー名変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
-  resource :user, only: %i[new create show edit update]
-  
+  get 'user/new', to: 'users#new'
+  get 'user', to: 'users#show'
+  get 'user/name', to: 'users#edit_name'
+  patch 'user/name', to: 'users#update_name'
+
+  # get 'users', to: "users#new"
+=begin
+  get 'users', to: "users#new"
+
+  resource :users
+  resource :user
+
+
+  get 'user/name', to: 'users#edit_name'
+  patch 'user/name', to: 'users#update_name'
+=end
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
