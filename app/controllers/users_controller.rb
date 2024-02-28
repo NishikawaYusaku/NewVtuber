63c7dc5655998e1b5ad19877b1f1dab452eddf91
NishikawaYusaku <sakurairo447@gmail.com>
2024-02-28 17:45:49 +0900

    [Add]規約同意のチェックボックス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,6 +79,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :agreement)
   end
 end
