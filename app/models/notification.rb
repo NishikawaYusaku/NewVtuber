6670512fe64e287751606afd1ae379e526149a73
NishikawaYusaku <sakurairo447@gmail.com>
2025-06-14 20:54:12 +0900

    [Add]モデル関連付け

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,7 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_aat: :desc)}
+  belongs_to :vtuber, optional: true
+  
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 end
