d9fa179ecc430238e489c8f5e6038c3c244c27d4
NishikawaYusaku <sakurairo447@gmail.com>
2025-07-13 15:47:15 +0900

    [Add]Notificationモデル > default_scope, association

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'default_scope' do
+    it '作成日時の降順で並んでいる' do
+      old = create(:notification, created_at: 2.days.ago)
+      new = create(:notification, created_at: 1.days.ago)
+      expect(Notification.all). to eq [new, old]
+    end
+  end
+
+  describe 'association' do
+    it 'vtuber' do
+      notification = build(:notification, vtuber: nil)
+      expect(notification).to be_valid
+    end
+    it 'visitor' do
+      notification = build(:notification, visitor: nil)
+      expect(notification).to be_valid
+    end
+    it 'visited' do
+      notification = build(:notification, visited: nil)
+      expect(notification).to be_valid
+    end
+    it 'visitor、visitedが同じクラスである' do
+      user = create(:user)
+      notification = build(:notification, visitor: user, visited: user)
+      expect(notification.visitor).to be_a(User)
+      expect(notification.visited).to be_a(User)
+    end
+  end
 end
