da30c5beb66899acc83e000d7d6dac4616a9a022
NishikawaYusaku <sakurairo447@gmail.com>
2025-07-13 16:41:53 +0900

    [Add]VtuberContentモデル > association

--- spec/models/vtuber_content_spec.rb
+++ spec/models/vtuber_content_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe VtuberContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:vtuber) {create(:vtuber)}
+  let(:content) {create(:content)}
+
+  describe 'association' do
+    context 'vtuber/content' do
+      it 'vtuber/contentがいる' do
+        vtuber_content = build(:vtuber_content, vtuber: vtuber, content: content)
+        expect(vtuber_content).to be_valid
+      end
+      it 'vtuberがいない' do
+        vtuber_content = build(:vtuber_content, vtuber: nil, content: content)
+        expect(vtuber_content).not_to be_valid
+      end
+      it 'contentがいない' do
+        vtuber_content = build(:vtuber_content, vtuber: vtuber, content: nil)
+        expect(vtuber_content).not_to be_valid
+      end
+    end
+  end
 end
