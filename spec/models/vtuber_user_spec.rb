b6a0679d114e8c1fbd4a0aa77b94e4ebf6a71ede
NishikawaYusaku <sakurairo447@gmail.com>
2025-07-13 17:46:14 +0900

    [Add]VtuberUserモデル > association

--- spec/models/vtuber_user_spec.rb
+++ spec/models/vtuber_user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe VtuberUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+  let(:vtuber) {create(:vtuber)}
+
+  describe 'association' do
+    context 'user/vtuber' do
+      it 'user/vtuberがいる' do
+        vtuber_user = build(:vtuber_user, user: user, vtuber: vtuber)
+        expect(vtuber_user).to be_valid
+      end
+      it 'userがいない' do
+        vtuber_user = build(:vtuber_user, user: nil, vtuber: vtuber)
+        expect(vtuber_user).not_to be_valid
+      end
+      it 'vtuberがいない' do
+        vtuber_user = build(:vtuber_user, user: user, vtuber: nil)
+        expect(vtuber_user).not_to be_valid
+      end
+    end
+  end
 end
