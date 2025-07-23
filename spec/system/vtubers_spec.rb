0d33e5c1b3ccbb38780bafd121e1d1bd43825194
NishikawaYusaku <sakurairo447@gmail.com>
2025-07-23 18:14:24 +0900

    [Add]Vtuber > ログイン前 > 検索他

--- spec/system/vtubers_spec.rb
+++ spec/system/vtubers_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe "Vtubers", type: :system do
     create(:vtuber_place, vtuber: vtuber2, place: place, url: "https://www.youtube.com/@hololive")
     create(:vtuber_place, vtuber: vtuber3, place: place, url: "https://www.youtube.com/@Vspo77")
     page.driver.browser.manage.window.resize_to(1400, 900)
+    visit root_path
   end
   context 'ログイン前' do
-    describe 'プロフィールページ',focus: true do
+    describe 'プロフィールページ' do
       before do
-        visit root_path
         expect(page).to have_content 'vtuber1'
         expect(page).to have_content 'vtuber2'
         expect(page).to have_content 'vtuber3'
@@ -40,22 +40,40 @@ RSpec.describe "Vtubers", type: :system do
     describe '検索' do
       context '該当プロフィールがある' do
         it 'プロフィールが表示される' do
-
+          fill_in 'q', with: 'vtuber1'
+          find('[data-testid="search"]').click
+          expect(page).to have_content 'vtuber1'
+          expect(page).not_to have_content 'vtuber2'
+          expect(page).not_to have_content 'vtuber3'
+          expect(page).not_to have_content '登録されていません'
         end
       end
       context '該当プロフィールがない' do
         it 'プロフィールが表示されない' do
-          
+          fill_in 'q', with: 'vtuber4'
+          find('[data-testid="search"]').click
+          expect(page).to have_content '登録されていません'
+          expect(page).not_to have_content 'vtuber'
         end
       end
     end
     it 'プロフィール作成ができない' do
-      
+      expect(page).not_to have_content '設定'
+      visit new_vtuber_path
+      expect(page).to have_content 'ログインしてください'
+      expect(current_path).to eq login_path
     end
     it 'プロフィール編集ができない' do
-      
+      expect(page).not_to have_content '設定'
+      visit edit_vtuber_path(vtuber1)
+      expect(page).to have_content 'ログインしてください'
+      expect(current_path).to eq login_path
     end
-    it 'ユーザページが見れない' do
+    it 'ユーザページが見れない',focus: true do
+      expect(page).not_to have_content 'マイページ'
+      visit user_path
+      expect(page).to have_content 'ログインしてください'
+      expect(current_path).to eq login_path
       
     end
   end
