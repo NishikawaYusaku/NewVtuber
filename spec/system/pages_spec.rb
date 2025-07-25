145efaf9d7c0c0ea8ef0b63b173990c407e9f633
NishikawaYusaku <sakurairo447@gmail.com>
2025-07-25 19:42:11 +0900

    [Add]Page > ログイン前

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :system do
-  context 'ログイン前' do
-    it 'サービス説明が見れる' do
+  before do
+    page.driver.browser.manage.window.resize_to(1400, 900)
+  end
 
+  context 'ログイン前', focus: true do
+    before do
+      visit root_path
+    end
+    it 'サービス説明が見れる' do
+      find("a[href='/pages/about']").click
+      expect(page).to have_content 'VTuberは好きですか？'
     end
     it 'お問い合わせが見れる' do
-
+      expect(page).to have_css('[data-testid="footer-query"]')
     end
     it '利用規約が見れる' do
-
+      find("a[href='/pages/terms']").click
+      expect(page).to have_content '本規約への同意'
     end
     it 'プライバシーポリシーが見れる' do
-
+      find("a[href='/pages/privacy_policy']").click
+      expect(page).to have_content 'お客様から取得する情報'
     end
   end
 
