92bf61282bc05ebe2b23ccc7e9c1df36857f3f76
NishikawaYusaku <sakurairo447@gmail.com>
2025-08-01 19:26:58 +0900

    [Change]マイページから設定モーダルを開いた時に入力欄が空であるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[edit_email update_email edit_name update_name]
 
   def show
-    @vtuber = current_user.favorite_vtubers.order("favorites.id DESC")
+    @favorites_vtuber = current_user.favorite_vtubers.order("favorites.id DESC")
   end
 
   def new
