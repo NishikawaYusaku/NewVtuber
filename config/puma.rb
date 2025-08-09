5851b386112ae6de1fdd5934049de2f29fe026a1
NishikawaYusaku <sakurairo447@gmail.com>
2025-08-09 20:19:03 +0900

    [Change]各ファイルの修正

--- config/puma.rb
+++ config/puma.rb
@@ -35,3 +35,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
