8aa34b59c99be39d39acdcd55df2ea485f66d455
NishikawaYusaku <sakurairo447@gmail.com>
2025-08-10 23:27:12 +0900

    [Change]各ファイルの修正

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -35,5 +35,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
--- config/schedule.rb
+++ config/schedule.rb
@@ -3,12 +3,32 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+# require File.expand_path(File.dirname(__FILE__) + '/environment')
+
+# set :environment, Rails.env
+# set :output, "#{Rails.root}/log/cron.log"
+
+
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 
+# job_type :rake, "source /Users/saku/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init - zsh)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+job_type :rake, %Q{export PATH="$HOME/.rbenv/shims:$PATH"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output} 
+
+# rails_env = ENV['RAILS_ENV'] || :development
+
+# set :environment, rails_env
 set :environment, Rails.env
 set :output, "#{Rails.root}/log/cron.log"
 
-# 1日1回（開発・本番とも同じスケジュール）
+
+
+
+
+# every 1.minutes do
 every 1.day, at: Rails.env.production? ? '6:00 pm' : '3:00 am' do
-  command "bash #{Rails.root}/run_rake_task.sh"
+  # # command "bash #{Rails.root}/run_rake_task.sh"
+  # command "env >> #{Rails.root}/log/cron_env.log"
+  # command "which ruby >> #{Rails.root}/log/cron_env.log"
+  # command "which bundle >> #{Rails.root}/log/cron_env.log"
+  rake "vtuber_youtube:update"
 end
