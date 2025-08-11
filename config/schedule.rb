1e17c02f569f374aba8d08912701f9a3c719e985
NishikawaYusaku <sakurairo447@gmail.com>
2025-08-11 16:18:41 +0900

    [Change]schedule.rbの修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,24 +13,48 @@
 
 # require File.expand_path(File.dirname(__FILE__) + '/environment')
 
-# job_type :rake, "source /Users/saku/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init - zsh)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
-job_type :rake, %Q{export PATH="$HOME/.rbenv/shims:$PATH"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output} 
+# # job_type :rake, "source /Users/saku/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init - zsh)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+# job_type :rake, %Q{export PATH="$HOME/.rbenv/shims:$PATH"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output} 
 
-# rails_env = ENV['RAILS_ENV'] || :development
+# # rails_env = ENV['RAILS_ENV'] || :development
 
-# set :environment, rails_env
-set :environment, Rails.env
-set :output, "#{Rails.root}/log/cron.log"
-
-
+# # set :environment, rails_env
+# set :environment, Rails.env
+# set :output, "#{Rails.root}/log/cron.log"
 
 
 
-# every 1.minutes do
-every 1.day, at: Rails.env.production? ? '6:00 pm' : '3:00 am' do
-  # command "bash #{Rails.root}/run_rake_task.sh"
-  # command "env >> #{Rails.root}/log/cron_env.log"
-  # command "which ruby >> #{Rails.root}/log/cron_env.log"
-  # command "which bundle >> #{Rails.root}/log/cron_env.log"
+
+
+# # every 1.minutes do
+# every 1.day, at: Rails.env.production? ? '6:00 pm' : '3:00 am' do
+#   # command "bash #{Rails.root}/run_rake_task.sh"
+#   # command "env >> #{Rails.root}/log/cron_env.log"
+#   # command "which ruby >> #{Rails.root}/log/cron_env.log"
+#   # command "which bundle >> #{Rails.root}/log/cron_env.log"
+#   rake "vtuber_youtube:update"
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require File.expand_path(File.dirname(__FILE__) + '/environment')
+
+job_type :rake, %Q{export PATH="$HOME/.rbenv/shims:$PATH"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output}
+
+rails_env = ENV['RAILS_ENV'] || 'development'
+set :environment, rails_env
+set :output, "log/cron.log"
+
+every 1.day, at: rails_env == 'production' ? '6:00 pm' : '3:00 am' do
   rake "vtuber_youtube:update"
 end
