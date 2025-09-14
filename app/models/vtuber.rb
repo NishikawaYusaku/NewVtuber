02c33a16c505d681a692c8c94f3f3f467adc1b7d
NishikawaYusaku <sakurairo447@gmail.com>
2025-09-14 21:14:57 +0900

    [Add]いいね数での絞り込み検索を追加

--- app/models/vtuber.rb
+++ app/models/vtuber.rb
@@ -22,9 +22,9 @@ class Vtuber < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    (super + ["birthday_month", "birthday_day", "debut_date_year", "debut_date_month", "debut_date_day"])
+    (super + ["birthday_month", "birthday_day", "debut_date_year", "debut_date_month", "debut_date_day", "favorites_count"])
   rescue NoMethodError
-    ["name", "affiliation", "gender", "birthday", "like", "unlike", "birthday_month", "birthday_day", "debut_date_year", "debut_date_month", "debut_date_day"]
+    ["name", "affiliation", "gender", "birthday", "like", "unlike", "birthday_month", "birthday_day", "debut_date_year", "debut_date_month", "debut_date_day", "favorites_count"]
   end
 
   def self.ransackable_associations(_auth_object = nil)
