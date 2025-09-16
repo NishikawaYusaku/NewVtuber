2f0c8f20d8e48a9a6382983a95c74bc1768e6398
NishikawaYusaku <sakurairo447@gmail.com>
2025-09-16 17:58:35 +0900

    [Change]params[:q]の削除をメソッド化して分離

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,23 +17,27 @@ class ApplicationController < ActionController::Base
       @results = Vtuber.joins(:tags).where(tags: { name: params[:tag] })
       @q_name = params[:tag]
     else
-      params[:q].delete(:places_name_eq) if params[:q][:places_name_eq].blank?
-      params[:q].delete(:birthday_month_eq) if params[:q][:birthday_month_eq].blank?
-      params[:q].delete(:birthday_day_eq) if params[:q][:birthday_day_eq].blank?
-      params[:q].delete(:debut_date_year_eq) if params[:q][:debut_date_year_eq].blank?
-      params[:q].delete(:debut_date_month_eq) if params[:q][:debut_date_month_eq].blank?
-      params[:q].delete(:debut_date_day_eq) if params[:q][:debut_date_day_eq].blank?
-      params[:q].delete(:favorites_count_gteq) if params[:q][:favorites_count_gteq].blank?
-      params[:q].delete(:favorites_count_lteq) if params[:q][:favorites_count_lteq].blank?
-      params[:q].delete(:vtuber_youtube_subscriber_count_gteq) if params[:q][:vtuber_youtube_subscriber_count_gteq].blank?
-      params[:q].delete(:vtuber_youtube_subscriber_count_lteq) if params[:q][:vtuber_youtube_subscriber_count_lteq].blank?
-      params[:q].delete(:vtuber_youtube_video_count_gteq) if params[:q][:vtuber_youtube_video_count_gteq].blank?
-      params[:q].delete(:vtuber_youtube_video_count_lteq) if params[:q][:vtuber_youtube_video_count_lteq].blank?
+      delete_blank_params
       @results = @q.result(distinct: true)
       @q_name = params[:q].values[0] if params[:q].values[0] != "" && params[:q][:filtering_search].blank?
       @filtering_search = params[:q][:filtering_search] if params[:q].keys[0] != "filtering_search"
     end
     @vtubers = @results.order(:id).page(params[:page]).per(20)
+  end
+
+  def delete_blank_params
+    params[:q].delete(:places_name_eq) if params[:q][:places_name_eq].blank?
+    params[:q].delete(:birthday_month_eq) if params[:q][:birthday_month_eq].blank?
+    params[:q].delete(:birthday_day_eq) if params[:q][:birthday_day_eq].blank?
+    params[:q].delete(:debut_date_year_eq) if params[:q][:debut_date_year_eq].blank?
+    params[:q].delete(:debut_date_month_eq) if params[:q][:debut_date_month_eq].blank?
+    params[:q].delete(:debut_date_day_eq) if params[:q][:debut_date_day_eq].blank?
+    params[:q].delete(:favorites_count_gteq) if params[:q][:favorites_count_gteq].blank?
+    params[:q].delete(:favorites_count_lteq) if params[:q][:favorites_count_lteq].blank?
+    params[:q].delete(:vtuber_youtube_subscriber_count_gteq) if params[:q][:vtuber_youtube_subscriber_count_gteq].blank?
+    params[:q].delete(:vtuber_youtube_subscriber_count_lteq) if params[:q][:vtuber_youtube_subscriber_count_lteq].blank?
+    params[:q].delete(:vtuber_youtube_video_count_gteq) if params[:q][:vtuber_youtube_video_count_gteq].blank?
+    params[:q].delete(:vtuber_youtube_video_count_lteq) if params[:q][:vtuber_youtube_video_count_lteq].blank?
   end
 
   def check_notification
