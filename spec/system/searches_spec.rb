16b21bbc8059f39b64fdc05bf4855f6a437e35cb
NishikawaYusaku <sakurairo447@gmail.com>
2025-09-17 19:38:10 +0900

    [Add]検索のシステムスペックの追加とコードの移行

---
+++ spec/system/searches_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe "Searches", type: :system do
+  let!(:vtuber1) { create(:vtuber, name: "vtuber1", name_x: "x1") }
+  let!(:vtuber2) { create(:vtuber, name: "vtuber2", name_x: "x2") }
+  let!(:vtuber3) { create(:vtuber, name: "vtuber3", name_x: "x3") }
+  let!(:place)   { create(:place, id: 1, name: "YouTube") }
+
+  before do
+    create(:vtuber_place, vtuber: vtuber1, place: place, url: "https://www.youtube.com/@nijisanji")
+    create(:vtuber_place, vtuber: vtuber2, place: place, url: "https://www.youtube.com/@hololive")
+    create(:vtuber_place, vtuber: vtuber3, place: place, url: "https://www.youtube.com/@Vspo77")
+    page.driver.browser.manage.window.resize_to(1400, 900)
+    visit root_path
+  end
+
+  context 'ログイン前' do
+    describe '検索' do
+      context '該当プロフィールがある' do
+        it 'プロフィールが表示される' do
+          find('[data-testid="before-login-normal-search-field"]').set('vtuber1')
+          find('[data-testid="before-login-normal-search-button"]').click
+          expect(page).to have_content 'vtuber1'
+          expect(page).not_to have_content 'vtuber2'
+          expect(page).not_to have_content 'vtuber3'
+          expect(page).not_to have_content '登録されていません'
+        end
+      end
+
+      context '該当プロフィールがない' do
+        it 'プロフィールが表示されない' do
+          find('[data-testid="before-login-normal-search-field"]').set('vtuber4')
+          find('[data-testid="before-login-normal-search-button"]').click
+          expect(page).to have_content '登録されていません'
+          expect(page).not_to have_content 'vtuber'
+        end
+      end
+    end
+  end
+
+  context 'ログイン後' do
+    before { login }
+
+    describe '検索' do
+      context '該当プロフィールがある' do
+        it 'プロフィールが表示される' do
+          find('[data-testid="after-login-normal-search-field"]').set('vtuber1')
+          find('[data-testid="after-login-normal-search-button"]').click
+          expect(page).to have_content 'vtuber1'
+          expect(page).not_to have_content 'vtuber2'
+          expect(page).not_to have_content 'vtuber3'
+          expect(page).not_to have_content '登録されていません'
+        end
+      end
+
+      context '該当プロフィールがない' do
+        it 'プロフィールが表示されない' do
+          find('[data-testid="after-login-normal-search-field"]').set('vtuber4')
+          find('[data-testid="after-login-normal-search-button"]').click
+          expect(page).to have_content '登録されていません'
+          expect(page).not_to have_content 'vtuber'
+        end
+      end
+    end
+  end
+end
