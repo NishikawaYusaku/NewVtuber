42f5565549dce67656754213552d62649d57461c
NishikawaYusaku <sakurairo447@gmail.com>
2025-10-25 10:26:54 +0900

    [Add]vtubersテーブルにカラムheight（身長）を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_17_121259) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_24_051738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -155,6 +155,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_17_121259) do
     t.integer "version", default: 0, null: false
     t.integer "favorites_count", default: 0
     t.string "language"
+    t.integer "height"
     t.index ["frequency_id"], name: "index_vtubers_on_frequency_id"
     t.index ["name"], name: "index_vtubers_on_name", unique: true
   end
