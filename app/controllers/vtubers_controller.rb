50a55359837fc35b05d98ecc8f5ee833fefebf27
NishikawaYusaku <sakurairo447@gmail.com>
2026-02-26 08:57:06 +0900

    [Update]Vプロフィールの作成ページでname_xで@をつけた状態でも@を外して保存されるように変更

--- app/controllers/vtubers_controller.rb
+++ app/controllers/vtubers_controller.rb
@@ -52,6 +52,8 @@ class VtubersController < ApplicationController
 
     @youtube_channel_id = is_platform_youtube
 
+    params[:vtuber][:name_x].slice!(0) if params[:vtuber][:name_x][0] == "@"
+
     if @vtuber.save
       VtuberUser.new(user_id: current_user.id, vtuber_id: @vtuber.id).save
 
