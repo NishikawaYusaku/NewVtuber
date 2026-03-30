fda4c4fa1a86e7735db54e26a7c5f5c661fa6ac2
NishikawaYusaku <sakurairo447@gmail.com>
2026-03-30 17:57:13 +0900

    [Update]Vプロフィールの設定モーダルを開いた時に、初めからお名前のテキストフィールドにフォーカスが当たっている状態にする

---
+++ app/assets/javascripts/modal_profile.js
@@ -0,0 +1,10 @@
+document.addEventListener("turbolinks:load", () => {
+  const modal = document.getElementById("exampleModal");
+  const input = document.getElementById("name-setting");
+
+  if (!modal || !input) return;
+
+  modal.addEventListener("shown.bs.modal", () => {
+    input.focus();
+  });
+});
