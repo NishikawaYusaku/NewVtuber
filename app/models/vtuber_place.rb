61f3db9b615b695292c43122337422b4b57df5e8
NishikawaYusaku <sakurairo447@gmail.com>
2026-05-03 18:24:25 +0900

    [Fix]Vプロフィールの配信サイトで他配信者の別種類のURLを入力した際に登録できてしまう問題の修正（作成）/編集の処理変更

--- app/models/vtuber_place.rb
+++ app/models/vtuber_place.rb
@@ -2,5 +2,5 @@ class VtuberPlace < ApplicationRecord
   belongs_to :vtuber
   belongs_to :place
 
-  validates :url, uniqueness: true, presence: true, inclusion: {in: ['https://'], message: "は「https://」から入力してください"}
+  validates :url, uniqueness: true, presence: true, format: { with: /\Ahttps:\/\//, message: "は「https://」から入力してください" }
 end
