574f5f56ac77e30bc9d1c8551ad49d578bddb266
NishikawaYusaku <sakurairo447@gmail.com>
2026-05-05 10:57:07 +0900

    [Add]VtuberYoutubeのモデルスペックの内容追加

--- spec/models/vtuber_youtube_spec.rb
+++ spec/models/vtuber_youtube_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.describe VtuberYoutube, type: :model do
   let(:vtuber) { create(:vtuber) }
 
+  describe 'validation' do
+    let(:vtuber_youtube) { build(:vtuber_youtube) }
+
+    context 'channel_id' do
+      it '有効な入力値' do
+        expect(vtuber_youtube).to be_valid
+      end
+
+      it '重複していない' do
+        create(:vtuber_youtube, channel_id: "UCX7YkU9nEeaoZbkVLVajcMg")
+        vtuber_youtube.channel_id = "UCX7YkU9nEeaoZbkVLVajcMg"
+        vtuber_youtube.valid?
+        expect(vtuber_youtube.errors[:channel_id]).to include "はすでに存在します"
+      end
+    end
+  end
+
   describe 'association' do
     context 'vtuber' do
       it 'vtuberがいる' do
