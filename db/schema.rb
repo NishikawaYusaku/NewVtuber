ba3a00ecb84de2932fd24bc7fa74194ba5f57cb5
NishikawaYusaku <sakurairo447@gmail.com>
2026-06-07 19:41:05 +0900

    [Update]Notificationテーブルにread_atカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_08_094738) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_07_103224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_08_094738) do
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "read_at"
+    t.index ["read_at"], name: "index_notifications_on_read_at"
     t.index ["visited_id"], name: "index_notifications_on_visited_id"
     t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
     t.index ["vtuber_id"], name: "index_notifications_on_vtuber_id"
