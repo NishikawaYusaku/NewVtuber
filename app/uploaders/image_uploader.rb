174b38f2e8484493dea7c5df74782b9f17c94937
NishikawaYusaku <sakurairo447@gmail.com>
2026-06-23 11:56:13 +0900

    [Change]テスト実行時に作成された画像を最後に削除するように変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    base_dir = Rails.env.test? ? "tmp/uploads" : "uploads"
+    "#{base_dir}/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
