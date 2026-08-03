6b19b6442c8f36d75932aaefc1f4d067df6bcfb0
NishikawaYusaku <sakurairo447@gmail.com>
2026-08-03 14:47:39 +0900

    [Change]Vプロフィール設定でXの文字数上限のバリデーションを設定

--- app/models/vtuber.rb
+++ app/models/vtuber.rb
@@ -68,7 +68,7 @@ class Vtuber < ApplicationRecord
   has_one :vtuber_youtube, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
-  validates :name_x, uniqueness: true, if: -> { new_record? || changes[:name_x] }, allow_blank: true
+  validates :name_x, uniqueness: true, length: { maximum: 15 }, if: -> { new_record? || changes[:name_x] }, allow_blank: true
 
   accepts_nested_attributes_for :vtuber_places, allow_destroy: true
 
